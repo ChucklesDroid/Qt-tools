62a4fe7029f9505ca14ac825ab3b598c56af695f
Aakarsh MJ <mj.akarsh@gmail.com>
2022-12-16 13:25:27 +0530

    Reconciling local repositories

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,10 +1,9 @@
-#include <iostream>
 #include <QtQuick>
 
 int main( int argc, char *argv[])
 {
-	QtGuiApplication app(argc, argv);
-	QtQuickView view;
+	QGuiApplication app(argc, argv);
+	QQuickView view;
 	view.setSource(QUrl("main.qml");
 	view.show();
 	return app.exec();
